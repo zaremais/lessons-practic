6cb7cf1cdddcc43510abafa677f405542d172538
zaremais <zaremais2807@gmail.com>
2022-10-10 15:07:21 +0600

    first commit

---
+++ lessonpractic.dart
@@ -0,0 +1,180 @@
+void main() async {  
+ 
+num a= 7;
+num b = 5.2;
+num result = a + b;
+num sum = (1 + 3);
+num price = a + b;
+a = 1;
+b = 9;
+
+int car  = 5;
+int d = 7;
+int book = 20;
+int summa = (16 + 20);
+int current = 0;
+
+double numberAll = 5.6;
+double resultA = 55;
+double normTemperat = 36.6;
+double sumDouble = (7.9 + 8.8);
+double pi = 3.14;
+
+String name = 'Zarema';
+String adress = '123456';
+String animal = 'cat'; 
+String msg = 'Привет всем, студентам!';
+String bookAll = ('classic');
+
+List form = [1, 2, 3];
+List w = [1, 'Hello', true];
+List sotrydnic = ['Директор', 'Экспедитор', 'Продавец'];
+List c = [23, 24, 25, true];
+List library = ['classic', 123, true];
+
+Map formula = {
+  'd': 3,
+  'c': 4,
+  'e': 5,
+};
+
+Map students = {
+  1: 'Ivan',
+  2: 'Petr',
+  3: 'Alex',
+};
+
+ Map y = {
+  1: 'Hello',
+  2: true
+ }; 
+
+
+Map number = {
+  1: 3,
+  2: 4,
+  3: 5};
+
+
+Map<String, int> resultAll = {
+  '1': 34,
+  '2': 45,
+  '3': 55,
+};
+
+Runes input = new Runes(
+  '\u2665  \u{1f605} \u{1f60e} \u{1f47b} \u{1f596} \u{1f44d}');
+
+const string = "Dart";
+final runes = string.runes.toList();  
+
+Runes emojMan = new Runes(' \u{2665}');
+
+Runes inpu = new Runes (' \u{1f605}');
+
+var heart_rune = '\u2665';
+  
+
+
+final res = a + 5;
+a = 2;
+
+final people = {
+'Ivan Ivanov',
+'Petr Petrov',
+'Alex Alexseev',
+};
+
+final numer = {
+  a = 3,
+  b = 4,
+  
+};
+
+final h = 4;
+
+final list4 = [1, 2, 3, 'home', true];
+
+const server = "https:// google.com";
+const telephone = 22334455;
+const numCar = ['asd', 555, true];
+const price1 = 15;
+const stroka = 'Flutter';
+
+dynamic sum2 = a + b;
+dynamic age = 'возраст';
+dynamic index = 720001;
+dynamic listLibrary = [1, 2, 3, 'список', true];
+dynamic mapSpisok = {
+  a: 1,
+  b: 2,
+};
+
+var sum3 = 4 + 7;
+var index1 = 15/3;
+var car1 = [1, 'asd', true];
+var car2 = [1, 2, 3, 4, 5];
+var car3 = {
+  1: car1,
+  2: car2,
+  3: car,
+};
+
+
+
+print(a); 
+print(b);
+print(result);
+print(sum);
+print(price);
+print(car);
+print(d);
+print(book);
+print(summa);
+print(current);
+print(numberAll);
+print(resultA);
+print(normTemperat);
+print(sumDouble);
+print(pi);
+print(name);
+print(adress);
+print(animal);
+print(msg);
+print(bookAll);
+print(form);
+print(w);
+print(sotrydnic);
+print(c);
+print(library);
+print(formula);
+print(students);
+print(y);
+print(number);
+print(resultAll);
+print(new String.fromCharCodes(input));
+print(new String.fromCharCodes(runes));
+print(new String.fromCharCodes(emojMan));
+print(new String.fromCharCodes(inpu));
+print(heart_rune);
+print(res);
+print(people);
+print((numer));
+print(h);
+print(list4);
+print(server);
+print(telephone);
+print(numCar);
+print(price1);
+print(stroka);
+print(sum2);
+print(age);
+print(index);
+print(listLibrary);
+print(mapSpisok);
+print(sum3);
+print(index1);
+print(car1);
+print(car2);
+print(car3);
+ }
